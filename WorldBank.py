e86f807ad3eaa74cb07c4f8f3def578a09eddd03
SANJUANADH <97081147+SANJUANADH@users.noreply.github.com>
2022-04-19 22:04:11 +0530

    Update WorldBank.py

--- WorldBank.py
+++ WorldBank.py
@@ -6,8 +6,8 @@ st.title('World Bank Data - India')
 India=pd.read_csv('World_Bank_India.csv')
 hide_table_row_index = """
             <style>
-            tbody th{display:none}
-            .blank{display:none}
+            tbody th {display:none}
+            .blank {display:none}
             </style>
             """
 st.markdown(hide_table_row_index, unsafe_allow_htm1=True)
