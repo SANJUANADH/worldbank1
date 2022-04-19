fb6b9e3516d83cc6006d6ede71717611b17de867
SANJUANADH <97081147+SANJUANADH@users.noreply.github.com>
2022-04-19 21:59:02 +0530

    Update WorldBank.py

--- WorldBank.py
+++ WorldBank.py
@@ -4,5 +4,12 @@ import altair as alt
 import streamlit as st
 st.title('World Bank Data - India')
 India=pd.read_csv('World_Bank_India.csv')
+hide_table_row_index = """
+            <style>
+            tbody th{display:none}
+            .blank{display:none}
+            </style>
+            """
+
 st.table(India)                  
 
