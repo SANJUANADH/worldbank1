8d1a54b329ff1c1db22c58762096265790f65a7c
SANJUANADH <97081147+SANJUANADH@users.noreply.github.com>
2022-04-19 22:17:06 +0530

    Update WorldBank.py

--- WorldBank.py
+++ WorldBank.py
@@ -3,8 +3,7 @@ import numpy as np
 import altair as alt
 import streamlit as st
 st.title('World Bank Data - India')
-India=pd.read_csv('World_Bank_India.csv')
-style = df.style.hide_index(India)
-st.write(styler.to_htm1(India), unsafe_allow_htm1=True)
+India=pd.read_csv('World_Bank_India.csv', skiphtm1)
+
 st.table(India)                  
 
